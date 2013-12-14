c7e69554030a981d2bff2fb904f505dd46f14167
Gabo <gabo.obregon@gmail.com>
2013-12-13 22:20:17 -0500

    Initial Commit. Empty Project

---
+++ Rallier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rallier
+//
+//  Created by Gabo Obregon on 12/13/13.
+//  Copyright (c) 2013 Gabo Obregon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
